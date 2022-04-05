8cc57fd3518ed42af41728aca6a4913296437e11
dennis0914 <dennis0914@naver.com>
2022-04-06 01:15:43 +0900

    ADD: access context

--- try_django/pages/views.py
+++ try_django/pages/views.py
@@ -6,9 +6,13 @@ def home_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Hello World!</h1>")
     return render(request, "home.html", {})
 
-def home_view(request, *args, **kwargs):
+def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
 
-def home_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text" : "This is about us",
+        "my_number" : "123",
+    }
+    return render(request, "about.html", my_context)
 
